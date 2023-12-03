305f46a3f1b09d0399cfd3c80b281d33cf879fe4
Nick <nicksweetj@gmail.com>
2023-12-02 22:08:11 -0500

    styled active link in nav bar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,23 +1,33 @@
+"use client"
 import React from 'react'
 import Link from 'next/link'
 import { PiBugBeetleBold } from "react-icons/pi";
+import { usePathname } from 'next/navigation';
+import classnames from 'classnames';
 
 const NavBar = () => {
+    const currentPath = usePathname();
 
-const link = [
-    { label: 'Dashboard', href: '/'},
-    { label: 'Issues', href: '/'}
-];
+    const link = [ //setting like array to our link paths and labels to avoid dupilcate code
+        { label: 'Dashboard', href: '/'},
+        { label: 'Issues', href: '/issues'}
+    ];
 
 
   return (
     <nav className='flex space-x-6 border-b mb-5 px-5 h-14 items-center'>
         <Link href="/"><PiBugBeetleBold /></Link>
         <ul className='flex space-x-6'>
+            {/* rendering the navbar compents by mapping through the link array */}
             {link.map(link =>
                 <Link key={link.href} 
-                className="text-zinc-500 hover:text-zinc-900 transition-colors"
-                href="{link.href}">
+                // using classnames library to create a function that returns a string of the classes to render 
+                className={classnames({
+                    'text-zinc-900': link.href === currentPath,
+                    'text-zinc-500': link.href !== currentPath,
+                    'hover:text-zinc-900 transition-colors': true,
+                })} 
+                href={link.href}>
                     {link.label}
                 </Link>
                 )}
