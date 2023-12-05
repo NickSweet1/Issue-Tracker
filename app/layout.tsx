a51f7bf3fcd2f550b8af14a68828a1de2ca41e4e
Nick <nicksweetj@gmail.com>
2023-12-05 13:26:42 -0500

    add a delete button and styled for screen size

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,36 +1,38 @@
-import '@radix-ui/themes/styles.css';
-import './theme-config.css';
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import NavBar from './NavBar'
-import '@radix-ui/themes/styles.css';
-import { Theme, ThemePanel } from '@radix-ui/themes';
+import "@radix-ui/themes/styles.css";
+import "./theme-config.css";
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import NavBar from "./NavBar";
+import "@radix-ui/themes/styles.css";
+import { Container, Theme, ThemePanel } from "@radix-ui/themes";
 
-const inter = Inter({ 
-  subsets: ['latin'],  
-  variable: '--font-inter',
- });
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter",
+});
 
 export const metadata: Metadata = {
-  title: 'Issue Tracker',
-  description: 'An application to track bugs and issues in projects.',
-}
+  title: "Issue Tracker",
+  description: "An application to track bugs and issues in projects.",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={inter.variable}>
         <Theme accentColor="grass" radius="large">
           <NavBar />
-          <main className='p-5'>{children}</main>
-          {/* <ThemePanel /> use this to pull up the theme panel menu*/} 
+          <main className="p-5">
+            <Container>{children}</Container>
+          </main>
+          {/* <ThemePanel /> use this to pull up the theme panel menu*/}
         </Theme>
       </body>
     </html>
-  )
+  );
 }
